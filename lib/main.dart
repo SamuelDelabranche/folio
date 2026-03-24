26cd7114e3a6a8a067863a39187b837dcc158eea
Samuel Delabranche <118735723+SamuelDelabranche@users.noreply.github.com>
2026-03-24 16:16:34 +0100

    feat: création de la LibraryPage et branchement dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:folio/features/library/library_page.dart';
 import 'app/theme.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
+      home: LibraryPage(),
     );
   }
 }
